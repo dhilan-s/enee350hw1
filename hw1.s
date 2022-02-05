2e7c94a1c235383bb65312ed2571b7d9cf5717c5
Dhilan Shah <dhilan@grace9.umd.edu>
2022-02-05 12:51:54 -0500

    Finished instructions, not tested

--- hw1.s
+++ hw1.s
@@ -1,8 +1,9 @@
 	.globl main
 	.text
 
-main:	# Use t1 for count of 1's
-	li	$t1, 0
+main:	# Initialize variables
+	li	$t1, 0		#loop counter
+	li	$t2, 0		#1's counter
 
 	# Print string msg1
 	li	$v0,4		# print_string syscall code = 4
@@ -14,7 +15,25 @@ main:	# Use t1 for count of 1's
 	syscall	
 	move	$t0,$v0		# syscall results returned in $v0
 
-loop:	
+loop:	addi	$t1, $t1, 1	# i = i + 1
+	sll	$t3, $t0, 1	# store first bit of number in temp
+	beq	$t3, 1, found	# if the bit is 1, branch to add to ones
+	blt	$t1, 36, loop	# repeat loop if count < 36
+
+exit:	li	$v0, 4
+	la	$a0, msg2
+	syscall
+
+	li	$v0, 1
+	move	$a0, $t2
+	syscall
+
+	li	$v0, 10
+	syscall
+
+found:	addi	$t2, $t2, 1
+	b 	loop
 
 	.data
 	msg1:	.asciiz	"Enter A:   "
+	msg2:	.asciiz "Number of 1s in A = "	
