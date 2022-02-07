d2107a59f43b4ca6d6d103a08894b1a921404576
Derek Tucker-Peters <dtuckerp@wireless-10-104-90-6.umd.edu>
2022-02-07 16:02:21 -0500

    should work

--- hw1.s
+++ hw1.s
@@ -1,39 +1,44 @@
 	.globl main
+
+    .data
+	msg1:	.asciiz	"Enter A:   "
+	msg2:	.asciiz "Number of 1s in A = "
+
 	.text
 
 main:	# Initialize variables
-	li	$t1, 0		#loop counter
-	li	$t2, 0		#1's counter
+	li	    $t1, 0		#loop counter
+	li	    $t2, 0		#1's counter
 
 	# Print string msg1
-	li	$v0,4		# print_string syscall code = 4
-	la	$a0, msg1	# load the address of msg
+	li	    $v0, 4		# print_string syscall code = 4
+	la	    $a0, msg1	# load the address of msg
 	syscall
 
 	# Get input A from user and save
-	li	$v0,5		# read_int syscall code = 5
+	li	    $v0, 5		# read_int syscall code = 5
 	syscall	
-	move	$t0,$v0		# syscall results returned in $v0
+	move    $t0, $v0		# syscall results returned in $v0
 
-loop:	addi	$t1, $t1, 1	# i = i + 1
-	sll	$t3, $t0, 1	# store first bit of number in temp
-	beq	$t3, 1, found	# if the bit is 1, branch to add to ones
-	blt	$t1, 36, loop	# repeat loop if count < 36
+loop:	
+    andi    $t4, $t0, 1 # t4 gets 1 if rightmost bit is 1 and 0 if not 
+	srl	    $t0, $t0, 1	# shift t0 right once
+    addi	$t1, $t1, 1	# i = i + 1
+    beq     $t4, 1, increment
+    blt	    $t1, 32, loop	# repeat loop if count < 32
 
-exit:	li	$v0, 4
-	la	$a0, msg2
+increment:
+    addi    $t2, $t2, 1 # increment 1's counter
+	blt	    $t1, 32, loop	# repeat loop if count < 32
+
+exit:	
+    li	    $v0, 4 # print string
+	la	    $a0, msg2 # prints msg2
 	syscall
 
-	li	$v0, 1
-	move	$a0, $t2
+	li	    $v0, 1 # prints integer
+	move	$a0, $t2 # print 1's counter
 	syscall
 
-	li	$v0, 10
+	li	    $v0, 10 # exit
 	syscall
-
-found:	addi	$t2, $t2, 1
-	b 	loop
-
-	.data
-	msg1:	.asciiz	"Enter A:   "
-	msg2:	.asciiz "Number of 1s in A = "	
